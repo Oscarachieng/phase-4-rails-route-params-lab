1d0b465e04da57ab171f3bacab6b32abee55f252
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-09-26 18:01:30 +0300

    commit tree 0da27ee8c8ac39d4cd46caf5c5af779d00287eef

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,12 @@ class StudentsController < ApplicationController
     students = Student.all
     render json: students
   end
+  
+  def show 
+    chosen_student = Student.find(
+    params[:id]
+    )
+    render json: chosen_student
+  end
 
 end
